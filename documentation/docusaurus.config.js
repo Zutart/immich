e0110b1c99383dfeb3b9405bf11c7d98a7816278
Alex <alex.tran1502@gmail.com>
2022-06-21 17:57:06 -0500

    update favicon

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.png",
+  favicon: "img/immich-logo.png",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
